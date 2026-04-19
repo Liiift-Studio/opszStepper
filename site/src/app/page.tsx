d46f0fb83b99ad5fcad2a754c1500f60cbf1ed62
Quinn Keaveney <qkeave@gmail.com>
2026-04-19 10:21:33 -0700

    Sync shared site-kit: globals.css to base.css pattern, SiteFooter component

--- site/src/app/page.tsx
+++ site/src/app/page.tsx
@@ -3,6 +3,8 @@ import CopyInstall from "@/components/CopyInstall"
 import CodeBlock from "@/components/CodeBlock"
 import ToolDirectory from "@/components/ToolDirectory"
 import { version } from "../../../package.json"
+import { version as siteVersion } from "../../package.json"
+import SiteFooter from "../components/SiteFooter"
 
 export default function Home() {
 	return (
@@ -109,15 +111,7 @@ const stop = startOpszStepper(el, { cuts })
 				</div>
 			</section>
 
-			{/* Footer */}
-			<footer className="w-full max-w-2xl lg:max-w-5xl flex flex-col gap-6 pt-8 border-t border-white/10 text-xs">
-				<ToolDirectory current="opszStepper" />
-				<hr className="border-white/10" />
-				<div className="grid grid-cols-2 sm:grid-cols-4 gap-x-8 opacity-50">
-					<a href="https://liiift.studio" className="hover:opacity-100 transition-opacity"><img src="/logo-white.png" alt="Liiift Studio" style={{ height: 18, display: 'block' }} /></a>
-					<span className="sm:col-start-4">opszStepper v{version}</span>
-				</div>
-			</footer>
+			<SiteFooter current="opszStepper" npmVersion={version} siteVersion={siteVersion} />
 
 		</main>
 	)
